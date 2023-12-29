0b2723c42cb35b98e48ceb4de2b25bd7d145daab
Name <Email>
2023-12-29 11:35:39 -0700

    chore: allowedExtensions on getFileFromFolder, delete excel file

--- apps/server/src/utils/getFileListFromFolder.ts
+++ apps/server/src/utils/getFileListFromFolder.ts
@@ -1,9 +1,23 @@
 import { ProjectFile } from 'ontime-types';
 
 import { readdirSync, statSync } from 'fs';
+import { extname } from 'path';
 
-export const getFileListFromFolder = (folderPath: string): Array<ProjectFile> => {
-  const files = readdirSync(folderPath);
+interface Options {
+  allowedExtensions?: Array<string>;
+}
+
+export const getFileListFromFolder = (folderPath: string, options: Options = {}): Array<ProjectFile> => {
+  let files = readdirSync(folderPath);
+
+  // Filter files if allowedExtensions is provided
+  if (options.allowedExtensions && options.allowedExtensions.length > 0) {
+    files = files.filter(file => {
+      const ext = extname(file).toLowerCase();
+      return options.allowedExtensions.includes(ext);
+    });
+  }
+
   return files.map((file) => {
     const filePath = `${folderPath}/${file}`;
     const stats = statSync(filePath);
